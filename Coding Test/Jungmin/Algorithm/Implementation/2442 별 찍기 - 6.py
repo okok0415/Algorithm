c99fd0e9c395d83b20589331b86308bbcabfaf1b
okok0415 <dlawjdals11@naver.com>
2022-03-07 18:24:17 +0900

    220307

---
+++ Coding Test/Jungmin/Algorithm/Implementation/2442 별 찍기 - 6.py
@@ -0,0 +1,13 @@
+import sys
+
+
+N = int(sys.stdin.readline())
+
+for i in range(N, 0, -1):
+    for j in range(N - i, 0, -1):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end="")
+    for j in range(i - 1):
+        print("*", end="")
+    print()
