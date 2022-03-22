d1f0f627d72671738bf7a6d7d63a15708ea1a8fd
okok0415 <dlawjdals11@naver.com>
2022-03-22 22:17:20 +0900

    220322 KAKAO

---
+++ Coding_Test/JavaScript/Algorithm/2022 KAKAO BLIND/신고결과받기.js
@@ -0,0 +1,36 @@
+function solution(id_list, report, k) {
+    var answer = [];
+    let dict = {}
+    let result = {}
+    for(i of id_list){
+        dict[i] = []
+        result[i] = []
+    }
+    
+    for(r of report){
+        let [x, y] = r.split(' ')
+        if(!dict[y].includes(x)){
+            dict[y].push(x)    
+        }
+    }
+    let stop = []
+    for(i of id_list){
+        if(dict[i].length >= k){
+            stop.push(dict[i])
+        }
+    }
+    for (s of stop){
+        for(a of s){
+
+            
+            result[a].push(1)
+               
+        }
+    }
+    result2 = []
+    for(i of id_list){
+        result2.push(result[i].length)
+    }
+    return result2;
+    
+}
